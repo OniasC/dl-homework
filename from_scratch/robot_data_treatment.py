32df52083e86e8e0e8d7f007a6e6c77a77bd7442
OCASTELO <onias.castelo@analog.com>
2024-10-02 18:54:37 +0100

    runs rnn for robot dataset

--- from_scratch/robot_data_treatment.py
+++ from_scratch/robot_data_treatment.py
@@ -27,12 +27,12 @@ def dataGet(path):
 
     data[' GT_VX'] = globalSpeedPostProcessingGet(data[' GT_X'],data[' TIMESTAMP'])
     data[' GT_VY'] = globalSpeedPostProcessingGet(data[' GT_Y'],data[' TIMESTAMP'])
-    data[' GT_W'] = globalSpeedPostProcessingGet(data[' GT_THETA'],data[' TIMESTAMP'])
+    data[' GT_W']  = globalSpeedPostProcessingGet(data[' GT_THETA'],data[' TIMESTAMP'])
 
-    # inputs will be only m1, m2, m3  and m4
+    # inputs will be only m1, m2, m3  and m4, theta
     inputs = []
     for index in range(len(data[' GT_W'])):
-        inputs.append(np.array([data[' ROBOT_M1'][index], data[' ROBOT_M2'][index], data[' ROBOT_M3'][index], data[' ROBOT_M4'][index]]))
+        inputs.append(np.array([data[' ROBOT_M1'][index], data[' ROBOT_M2'][index], data[' ROBOT_M3'][index], data[' ROBOT_M4'][index], data[' GT_THETA'][index]]))
     # outputs will gt_vx, gt_vy, and gt_w
     gt_outputs = []
     for index in range(len(data[' GT_W'])):
@@ -40,21 +40,20 @@ def dataGet(path):
 
     return data, inputs, gt_outputs
 
-_, x0, y0 = dataGet("data/quadrado_opt_1_1.csv")
-_, x1, y1 = dataGet("data/quadrado_opt_1_2.csv")
-_, x2, y2 = dataGet("data/quadrado_opt_1_3.csv")
-_, x3, y3 = dataGet("data/quadrado_opt_1_4.csv")
-_, x4, y4 = dataGet("data/quadrado_opt_1_5.csv")
-_, x5, y5 = dataGet("data/quadrado_opt_2_1.csv")
-_, x6, y6 = dataGet("data/quadrado_opt_2_2.csv")
-_, x7, y7 = dataGet("data/quadrado_opt_2_3.csv")
-_, x8, y8 = dataGet("data/quadrado_opt_2_4.csv")
-_, x9, y9 = dataGet("data/quadrado_opt_2_5.csv")
+#_, x0, y0 = dataGet("data/quadrado_opt_1_1.csv")
+#_, x1, y1 = dataGet("data/quadrado_opt_1_2.csv")
+#_, x2, y2 = dataGet("data/quadrado_opt_1_3.csv")
+#_, x3, y3 = dataGet("data/quadrado_opt_1_4.csv")
+#_, x4, y4 = dataGet("data/quadrado_opt_1_5.csv")
+#_, x5, y5 = dataGet("data/quadrado_opt_2_1.csv")
+#_, x6, y6 = dataGet("data/quadrado_opt_2_2.csv")
+#_, x7, y7 = dataGet("data/quadrado_opt_2_3.csv")
+#_, x8, y8 = dataGet("data/quadrado_opt_2_4.csv")
+#_, x9, y9 = dataGet("data/quadrado_opt_2_5.csv")
 
-print(len(x0))
-x0 = x0 #+ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9
-y0 = y0 #+ y1 + y2 + y3 + y4 + y5 + y6 + y7 + y8 + y9
-print(len(x0))
+
+#x0 = x0 + x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9
+#y0 = y0 + y1 + y2 + y3 + y4 + y5 + y6 + y7 + y8 + y9
 
 '''
 now that i have complete set of inputs and ground truth outputs, i want to break them down into arrays of size N to be fed to the training set
@@ -66,15 +65,23 @@ def createTestTrainSets(x, y, N, trainRatio = 0.6, valRatio = 0.2, testRatio = 0
 
     x_list = []
     y_list = []
-    print(int(len(x)/N))
-    print(trainSize, valSize, testSize)
+    #print(int(len(x)/N))
+    #print(trainSize, valSize, testSize)
     for iter in range(int(len(x)/N)):
         x_list.append(x[iter:iter+N])
         y_list.append(y[iter:iter+N])
     shuffle(x_list)
     shuffle(y_list)
 
+    trainTuple = []
+    valTuple = []
+    testTuple = []
+    for i in range(trainSize):
+        trainTuple.append((x_list[i], y_list[i]))
+    for i in range(valSize):
+        valTuple.append((x_list[i+trainSize], y_list[i+trainSize]))
+    for i in range(testSize):
+        testTuple.append((x_list[i+trainSize+valSize], y_list[i+trainSize+valSize]))
+    return trainTuple, valTuple, testTuple
 
-
-
-createTestTrainSets(x0, y0, 10)
+#trainTuple, valTuple, testTuple = createTestTrainSets(x0, y0, 10)
